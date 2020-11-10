df2eacb98928a3d4dd1beda9752547f1c4070265
aolyu <aolyu@qq.com>
2020-11-10 11:56:20 +0800

    feat: 新增websocket.js

--- cloud-notes-app/src/App.jsx
+++ cloud-notes-app/src/App.jsx
@@ -1,7 +1,10 @@
+import {createSocket} from './utils/websocket'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  createSocket('ws://192.168.1.4:8999')
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +12,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
