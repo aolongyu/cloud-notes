3a9fd9f4bdb7f750fac7c866ebc917a86d1697a0
aolyu <aolyu@qq.com>
2020-11-11 20:03:47 +0800

    Revert "尝试"
    This reverts commit 96b6ceabedfa3614d1df6533bd41b62405a9b43b.

--- cloud-notes-server/Server/src/snet/connection.go
+++ cloud-notes-server/Server/src/snet/connection.go
@@ -1,12 +1,14 @@
 package snet
 
 import (
-	"Settings"
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 	"isface"
 	"net"
+	"Settings"
 )
 
 //icconect的实现层
@@ -244,10 +246,27 @@ func (c *Connection) SendMesg(msgId uint32, data []byte) error {
 	}
 	c.msgChan <- msg
 	return nil*/
-	pack := NewDataPack()
-	msgChange := pack.Webpacksocket(data)
-	fmt.Println("发送消息",msgChange)
-	c.msgChan <- msgChange
+	if int32(len(data)) >= int32(Settings.GlobalObject.MaxPacketSize) {
+		return errors.New("发送的包长大于2048")
+	}
+
+	length := len(data)
+	maskedData := make([]byte,length)
+	for i:=0;i<length ;i++{
+		maskedData[i] = data[i]
+	}
+	dataBuf := bytes.NewBuffer([]byte{})
+	if err := binary.Write(dataBuf,binary.BigEndian,[]byte{0x81});err!=nil{
+		return err
+	}
+	payLenByte := byte(0x00) | byte(length)
+	if err := binary.Write(dataBuf,binary.BigEndian,payLenByte);err!=nil{
+		return err
+	}
+	if err := binary.Write(dataBuf,binary.BigEndian,dataBuf);err!=nil{
+		return err
+	}
+	c.msgChan <- dataBuf.Bytes()
 	return nil
 }
 
