c7a4c1cb0b8a51ea8b3343513a91d97fbe3f05af
aolyu <aolyu@qq.com>
2020-11-28 20:26:16 +0800

    登录

--- cloud-notes-app/src/pages/login/index.tsx
+++ cloud-notes-app/src/pages/login/index.tsx
@@ -21,10 +21,24 @@ const LoginPage: FC<PageProps> = ({ login, dispatch }) => {
   // 注意，上面这里写空数组，表示初始化，如果需要监听某个字段变化再发起请求，可以在这里写明
   const { name } = login;
   createSocket('ws://localhost:8999')
-  setTimeout(() => {
-    sendWSPush('login', '321')
-  }, 1000)
-  return <div className={styles.center}>Hello {name}</div>;
+  
+  // return <div className={styles.center}>Hello {name}</div>;
+
+  const handleClick = () => {
+    const Name = document.getElementById('name').value
+    const Password = document.getElementById('password').value
+    console.log(Name, Password)
+    const sendMsg = {Name, Password}
+    sendWSPush('login', JSON.stringify(sendMsg))
+  }
+
+  return (
+    <div className="container">
+      <input id='name' type="text" placeholder='请输入账号' value='aolyu@qq.com' /><br/>
+      <input id='password' type="password" placeholder='请输入密码' value='123' /><br/>
+      <input onClick={handleClick}  type="button" value="登录"/>
+    </div>
+  );
 };
 
 export default connect(({ login }: { login: LoginModelState; }) => ({ login }))(LoginPage);
