534b56e6cc0ed971d4c95363bae553e71d4e99c2
Cdw <1074537520@qq.com>
2020-11-29 13:25:15 +0800

    添加了登录的handle

--- cloud-notes-server/Server/src/shandle/login.go
+++ cloud-notes-server/Server/src/shandle/login.go
@@ -14,16 +14,21 @@ type LoginUserJson struct{
 	Name string `json:"Name"`
 	Password string `json:"Password"`
 }
-
+type Result struct{
+	result int32
+}
 func(T Login) Handle(request isface.IRequest){
 	conn := request.GetConnection()
 	Loginmessage := LoginUserJson{}
 
 	json.Unmarshal(request.GetData(),&Loginmessage)
-
+	//登录失败
+	res := &Result{result:0}
 	fmt.Println("Handle Login   传来的信息:姓名",Loginmessage.Name,"密码",Loginmessage.Password)
 
-	if Loginmessage.Name == "aolyu@qq.com" && Loginmessage.Password == "123" {
+	snet.SDB.Debug().Raw("call login(?,?)",Loginmessage.Name,Loginmessage.Password).Scan(res)
+
+	if res.result == 0 {
 		conn.SendMesg([]byte("loginack"), []byte("ok"))
 	}else{
 		conn.SendMesg([]byte("loginack"), []byte("no"))
